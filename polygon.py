50c3c3f35cddcc06f3a3c56b7384c8f399a331ef
Yudong Zhang <zyddawn1995@gmail.com>
2019-05-25 22:46:15 +0800

    Implement polygon drawing using 'DDA' or 'Bresenham'

--- polygon.py
+++ polygon.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from line import *
 
 class Polygon(QLabel):
 	def __init__(self, points=[], algorithm='Bresenham'):
@@ -32,23 +33,22 @@ class Polygon(QLabel):
 		return Res
 
 	def DDA(self):
-		print("DDA not implemented yet.")
-		pass
+		n = len(self.rawPoints)
+		for i in range(n):
+			p1, p2 = self.rawPoints[i], self.rawPoints[(i+1)%n]
+			newLine = Line(QPoint(p1[0], p1[1]), QPoint(p2[0], p2[1]), algorithm='DDA')
+			Res = newLine.getPoints()
+			if Res:
+				self.point_arr.extend(newLine.point_arr)
 
 	def Bresenham(self):
-		print("Bresenham not implemented yet.")
-		pass
-
-
-
-
-
-
-
-
-
-
-
+		n = len(self.rawPoints)
+		for i in range(n):
+			p1, p2 = self.rawPoints[i], self.rawPoints[(i+1)%n]
+			newLine = Line(QPoint(p1[0], p1[1]), QPoint(p2[0], p2[1]), algorithm='Bresenham')
+			Res = newLine.getPoints()
+			if Res:
+				self.point_arr.extend(newLine.point_arr)
 
 
 
