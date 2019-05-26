65b6bce9a1c3f1f0baa091f6daf58d5556121caf
Yudong Zhang <zyddawn1995@gmail.com>
2019-05-26 11:15:07 +0800

    Implement Liang-Barsky algorithm for line clipping

--- transform.py
+++ transform.py
@@ -52,6 +52,7 @@ def lineClip(lineElem, pLL, pUR, algorithm='Cohen-Sutherland'):	# lower_left, up
 
 
 def CohenSutherland(lineElem, pLL, pUR):
+	# print("CohenSutherland")
 	xLL, yLL = pLL.x(), pLL.y()
 	xUR, yUR = pUR.x(), pUR.y()
 	x0, y0 = lineElem.object.p1.x(), lineElem.object.p1.y()
@@ -59,13 +60,15 @@ def CohenSutherland(lineElem, pLL, pUR):
 	x, y = 0, 0
 	c, c1, c2 = 0, encode(xLL, yLL, xUR, yUR, x0, y0), \
 				encode(xLL, yLL, xUR, yUR, x2, y2)
+	# print(c1, c2)
 	if c1==0 and c2==0:
 		return ;
 	else:
 		while c1!=0 or c2!=0:
-			if c1 & c2 == 1:
+			if c1 & c2:
 				lineElem.object.setP1(QPoint(0, 0))
 				lineElem.object.setP2(QPoint(0, 0))
+				# print("No line left")
 				lineElem.updatePoints([])
 				return ;
 			c = c2 if c1==0 else c1
@@ -85,7 +88,8 @@ def CohenSutherland(lineElem, pLL, pUR):
 				c2 = encode(xLL, yLL, xUR, yUR, x, y)
 		lineElem.object.setP1(QPoint(x0, y0))
 		lineElem.object.setP2(QPoint(x2, y2))
-		lineElem.updatePoints([])
+		# print("from {} to {}".format((x0, y0), (x2, y2)))
+		lineElem.updatePoints([]) 
 
 
 def encode(xLL, yLL, xUR, yUR, x, y):
@@ -113,14 +117,56 @@ def encode(xLL, yLL, xUR, yUR, x, y):
 
 
 def LiangBarsky(lineElem, pLL, pUR):
-	print("LiangBarsky to be implemented.")
-	pass
+	xLL, yLL = pLL.x(), pLL.y()
+	xUR, yUR = pUR.x(), pUR.y()
+	x0, y0 = lineElem.object.p1.x(), lineElem.object.p1.y()
+	x2, y2 = lineElem.object.p2.x(), lineElem.object.p2.y()
+	t0 = t1 = deltax = deltay = 0
+	t0, t1 = 0.0, 1.0
+	deltax, deltay = x2 - x0, y2 - y0
+	Res = True
+	t_dict = {"t0":t0, "t1":t1}
+	print(t_dict)
+	if not canSeeLine(-deltax, x0-xLL, t_dict): 
+		Res = False
+	if Res and not canSeeLine(deltax, xUR-x0, t_dict):
+		Res = False
+	if Res and not canSeeLine(-deltay, y0-yLL, t_dict):
+		Res = False
+	if Res and not canSeeLine(deltay, yUR-y0, t_dict):
+		Res = False
+	if not Res:
+		lineElem.object.setP1(QPoint(0, 0))
+		lineElem.object.setP2(QPoint(0, 0))
+		lineElem.updatePoints([])
+	else:
+		t0, t1 = t_dict["t0"], t_dict["t1"]
+		x2, y2 = x0+t1*deltax, y0+t1*deltay
+		x0, y0 = x0+t0*deltax, y0+t0*deltay
+		print("from {} to {}".format((x0, y0), (x2, y2)))
+		lineElem.object.setP1(QPoint(x0, y0))
+		lineElem.object.setP2(QPoint(x2, y2))
+		lineElem.updatePoints([]) 
 
 
-
-
-
-
+def canSeeLine(q, d, t_dict):
+	r = 0
+	if q < 0:
+		r = d/q
+		if r > t_dict["t1"]:
+			return False
+		elif r > t_dict["t0"]:
+			t_dict["t0"] = r
+	elif q > 0:
+		r = d/q
+		if r < t_dict["t0"]:
+			return False
+		elif r < t_dict["t1"]:
+			t_dict["t1"] = r
+	elif d < 0:
+		return False
+	print(t_dict)
+	return True
 
 
 
