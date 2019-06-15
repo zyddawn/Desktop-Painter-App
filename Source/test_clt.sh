170b6c615feb009dd4f8a1a531c6a6d583bebfc5
Yudong Zhang <zyddawn1995@gmail.com>
2019-06-15 17:22:54 +0800

    Finish

--- Source/test_clt.sh
+++ Source/test_clt.sh
@@ -5,28 +5,25 @@ then
 	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/input.txt\""
 	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/input.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_line.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_line.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_line.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_line.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_polygon.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_polygon.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_polygon.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_polygon.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_ellipse.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_ellipse.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_new_ellipse.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_new_ellipse.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_translate.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_translate.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_translate.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_translate.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_rotate.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_rotate.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_rotate.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_rotate.txt"
 
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_scale.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_scale.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_scale.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_scale.txt"
 
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_clip.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_clip.txt"
+	echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_clip.txt\""
+	python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_clip.txt"
 	
-	# echo -e "\npython clt.py --path \"../CLT_test_scripts/res/\" --script \"../CLT_test_scripts/test_load.txt\""
-	# python clt.py --path "../CLT_test_scripts/res/" --script "../CLT_test_scripts/test_load.txt"
-
 fi
